2ab312d1fe695fcc4b76fb5e5a3ca0e82aa6e0b1
Barbara Michel <barbmichel22@gmail.com>
2024-07-09 16:18:23 +0000

    Choose random word from the word list

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 import random
 from wordlist import word_list
 
+print("**************************************")
+print("LET'S PLAY HANGMAN!   ****************")
+print("**************************************")
+name = input("What is your name?  ")
+print("\nHello " + name + ", let's get started.\n")
+
+word = random.choice(word_list)
+print("The hangman word is: " + word)
