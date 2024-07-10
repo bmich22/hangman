48660b89e53dc0daf4f73633724ee4c912ae0a4d
Barbara Michel <barbmichel22@gmail.com>
2024-07-10 21:06:26 +0000

    Added color to print statments and changed the hangman print statments

--- run.py
+++ run.py
@@ -2,6 +2,19 @@ import random
 import os
 from wordlist import word_list
 
+# Add colors
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
+ 
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
+ 
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
+ 
+def prPurple(skk): print("\033[95m {}\033[00m" .format(skk))
+ 
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
+ 
+
+
 # Choose a random word from the word list
 def choose_word():
     word = random.choice(word_list).upper()
@@ -43,23 +56,23 @@ def play_game(word):
     while not won and attempts < 6:
         # Create variable for player's guess
         print(display_hangman(attempts))
-        print("The word you're trying to guess has " + str(num_of_letters) + " letters.\n")
+        prCyan("The word you're trying to guess has " + str(num_of_letters) + " letters.\n")
         print(display_wordstring)
         print("\n")
         guess = input("Please guess a letter or you can try to guess the word.  ").upper()
         if guess.isalpha() and len(guess) == 1:
                 if guess in guessed_letters:
                     os.system('clear')
-                    print("\nYou already guessed the letter " + guess + ".")
+                    prCyan("\nYou already guessed the letter " + guess + ".")
                     print("\n")
                 elif guess not in word:
                     os.system('clear')
-                    print("\nIncorrect! There is no letter " + guess + " in the word.")
+                    prCyan("\nIncorrect! There is no letter " + guess + " in the word.")
                     guessed_letters.append(guess)
                     attempts += 1
                 else:
                     os.system('clear')
-                    print("\nCorrect! " + guess + " is in the word.")
+                    prCyan("\nCorrect! " + guess + " is in the word.")
                     guessed_letters.append(guess)
                     guessed_correct_letters.append(guess)
                     for position, letter in zip(positions, word_letters):
@@ -75,18 +88,18 @@ def play_game(word):
                         won=True
         elif guess.isalpha() and len(guess) > 1:
             if guess in guessed_words:
-                print("You already guessed this word.")
+                prCyan("You already guessed this word.")
             elif guess == word.upper():
-                print("Congrats! Your word guess is correct!")
+                prCyan("Congrats! Your word guess is correct!")
                 won = True
             else:
                 os.system('clear')
-                print("Your word guess is incorrect, please try again.")
+                prCyan("Your word guess is incorrect, please try again.")
                 guessed_words.append(guess)
                 attempts += 1
         else:
             os.system('clear')
-            print("Oops!!\nYou did not enter a letter or word, please try again.\n")
+            prCyan("Oops!!\nYou did not enter a letter or word, please try again.\n")
     game_over(word, won, attempts)
 
 # Display Hangman based on number of wrong attempts
@@ -94,130 +107,137 @@ def display_hangman(attempts):
     if attempts == 0:
         hangman_result = """
 ********  HANGMAN  **********
-*****************************
-***      -----------      ***
-***      |         |      ***
-***      |         |      ***
-***      |                ***
-***      |                ***
-***      |                ***
-***      |                ***
-***      |                ***
-***      |                ***
-***                       ***
-*** YOU HAVE SIX CHANCES  ***
+
+      -----------      
+      |         |      
+      |         |      
+      |                
+      |                
+      |                
+      |                
+      |                
+      |                
+                       
+    WRONG ANSWERS: 0 / 6
+
 *****************************
         """
     elif attempts == 1:
         hangman_result = """
-*****************************
-***      -----------      ***
-***      |         |      ***
-***      |         |      ***
-***      |        ( )     ***
-***      |                ***
-***      |                ***
-***      |                ***
-***      |                ***
-***      |                ***
-***                       ***
-*** YOU HAVE FIVE CHANCES ***
+********  HANGMAN  **********
+      -----------      
+      |         |      
+      |         |      
+      |        ( )     
+      |                
+      |                
+      |                
+      |                
+      |                
+                       
+    WRONG ANSWERS: 1 / 6 
+
 *****************************
         """
 
     elif attempts == 2:
         hangman_result = """
-*****************************
-***      -----------      ***
-***      |         |      ***
-***      |         |      ***
-***      |        ( )     ***
-***      |         |      ***
-***      |         |      ***
-***      |                ***
-***      |                ***
-***                       ***
-*** YOU HAVE FOUR CHANCES ***
-*****************************
-        """
+********  HANGMAN  **********
+      -----------      
+      |         |      
+      |         |      
+      |        ( )     
+      |         |      
+      |         |      
+      |                
+      |                
+                       
+    WRONG ANSWERS: 2 / 6 
+
+*****************************"""
 
     elif attempts == 3:
         hangman_result = r"""
-*****************************
-***      -----------      ***
-***      |         |      ***
-***      |         |      ***
-***      |        ( )     ***
-***      |        \|      ***
-***      |         |      ***
-***      |                ***
-***      |                ***
-***                       ***
-*** YOU HAVE THREE CHANCES **
-*****************************
-        """
+********  HANGMAN  **********
+      -----------      
+      |         |      
+      |         |      
+      |        ( )     
+      |        \|      
+      |         |      
+      |                
+      |                
+                       
+    WRONG ANSWERS: 3 / 6
+
+*****************************"""
 
     elif attempts == 4:
         hangman_result = r"""
-*****************************
-***      -----------      ***
-***      |         |      ***
-***      |         |      ***
-***      |        ( )     ***
-***      |        \|/     ***
-***      |         |      ***
-***      |                ***
-***      |                ***
-***                       ***
-***  YOU HAVE TWO CHANCES ***
-*****************************
-        """
+********  HANGMAN  **********
+      -----------      
+      |         |      
+      |         |      
+      |        ( )     
+      |        \|/      
+      |         |      
+      |                
+      |                
+                       
+    WRONG ANSWERS: 4 / 6 
+
+*****************************"""
          
 
     elif attempts == 5:
         hangman_result = r"""
-*****************************
-***      -----------      ***
-***      |         |      ***
-***      |         |      ***
-***      |        ( )     ***
-***      |        \|/     ***
-***      |         |      ***
-***      |        /       ***
-***      |                ***
-***                       ***
-** YOU HAVE ONE MORE CHANCE *
-*****************************
-        """
+********  HANGMAN  **********
+      -----------      
+      |         |      
+      |         |      
+      |        ( )     
+      |        \|/      
+      |         |      
+      |        /       
+      |              
+                       
+    WRONG ANSWERS: 5 / 6
+
+*****************************"""
 
     else:
         hangman_result = r"""
-*****************************
-***      -----------      ***
-***      |         |      ***
-***      |         |      ***
-***      |        ( )     ***
-***      |        \|/     ***
-***      |         |      ***
-***      |        / \     ***
-***      |                ***
-***                       ***
-***   SORRY, YOU LOST!    ***
-*****************************
-        """
+********  HANGMAN  **********
+      -----------      
+      |         |      
+      |         |      
+      |        ( )     
+      |        \|/      
+      |         |      
+      |        / \      
+      |              
+                       
+WRONG ANSWERS: 6/6  YOU LOST!
+
+*****************************"""
     return hangman_result
 
 # Show win or lost to player
 def game_over(word, won, attempts):
     if attempts >= 6:
         os.system('clear')
-        print("\n")
-        print("GAME OVER!\nYou lost.\n\nThe word was " + word + ".")    
-        print(display_hangman(attempts))
+        print("\n ")
+        prCyan("GAME OVER!\n")
+        prCyan("You lost.\n The word was " + word + ".")    
+        prRed(display_hangman(attempts))
         continue_or_quit()
     if won:
         os.system('clear')
-        print("""
+        print("\n ")
+        prCyan("WELL DONE!\n")
+        prCyan("The word was " + word +".\n")
+        prCyan("Congratulations, you got the word!\n")
+        prPurple("""
 ********  HANGMAN  **********
 *****************************
 ***                       ***
@@ -228,49 +248,69 @@ def game_over(word, won, attempts):
 *****************************
 *****************************
         """)
-        print("The word was " + word +".\n")
-        print("Congratulations, you got the word!\n")
         continue_or_quit()
 
 # Ask player to continue playing or quit
 def continue_or_quit():
     play_again = False
     while not play_again:
-        reply = input("Would you like to play again?\nPlease enter Y to play again and N to quit.").upper()
+        reply = input("\nWould you like to play again?\nPlease enter Y to play again and N to quit.").upper()
         if reply == "Y":
             os.system('clear')
             play_again = True
             main()
         elif reply == "N":
-            print("\nThank you for playing today!\nGoodbye!\n")
+            prCyan("\nThank you for playing today!\nGoodbye!\n")
             exit()
         else:
             print("\nYour entry was invalid.\n")
 
 # Main function    
 def main():
-    print("""
-\n
-*****************************    
-LET'S PLAY A              ***
-GAME OF HANGMAN.          ***
-*****************************
-***      -----------      ***
-***      |         |      ***
-***      |         |      ***
-***      |                ***
-***      |                ***
-***      |                ***
-***      |                ***
-***      |                ***
-***      |                ***
-***                       ***
+#     print("""
+# \n
+# *****************************    
+# LET'S PLAY A              ***
+# GAME OF HANGMAN.          ***
+# *****************************
+# ***      -----------      ***
+# ***      |         |      ***
+# ***      |         |      ***
+# ***      |                ***
+# ***      |                ***
+# ***      |                ***
+# ***      |                ***
+# ***      |                ***
+# ***      |                ***
+# ***                       ***
+# *****************************\n""")
+#     prCyan("""
+# \n
+# *****************************""") 
+    print("""\n
+LET'S PLAY A GAME OF HANGMAN.
+""")
+    prCyan("""
+*****************************""") 
+    prRed("""
+      -----------      
+      |         |      
+      |         |      
+      |                
+      |                
+      |                
+      |                
+      |                
+      |    
+    """)            
+    
+    prCyan("""                        
 *****************************\n""")
     name = input("Please enter your name:  ")
     os.system('clear')
-    print("\nHello, " + name.capitalize() + ".\n")
-    print("If you make six wrong guesses, you lose!\n")
-    print("Let's begin.")
+    prCyan("\nHello, " + name.capitalize() + ".\n")
+    prCyan("If you make six wrong guesses, you lose!\n")
+    prCyan("Let's begin.")
     word = choose_word()
     play_game(word)
 
