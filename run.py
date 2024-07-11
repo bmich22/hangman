3e0ae33bd1ac00bc1ba18274c5390bf3c84ba09c
Barbara Michel <barbmichel22@gmail.com>
2024-07-11 12:07:05 +0000

    Fix loop to play again so the same word is not repeated

--- run.py
+++ run.py
@@ -2,13 +2,16 @@ import random
 import os
 from wordlist import word_list
 
-# Add colors
+# Add colors 
 def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
 
 def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
 
 def prPurple(skk): print("\033[95m {}\033[00m" .format(skk))
 
+"""
+Get the name of the new player 
+"""
 def get_name():
     print("""\n
 LET'S PLAY A GAME OF HANGMAN.
@@ -30,29 +33,34 @@ LET'S PLAY A GAME OF HANGMAN.
     prCyan("""                        
 *****************************\n""")
     new_name = input("Please enter your name:  ")
-    # prCyan("\nHello, " + new_name.capitalize() + ".\n")
-    # prCyan("If you make six wrong guesses, you lose!\n")
-    # prCyan("Let's begin.")
     return new_name
 
+""" 
+Message to new player  
+"""
 def intro_message():
     os.system('clear')
-    prCyan("\nHello, " + name.capitalize() + ".\n")
-    prCyan("Guess letters or the whole word!\n")
+    prCyan("\nHello, " + name.capitalize() + ".")
+    prCyan("Guess the letters or the whole word.")
     prCyan("If you make six wrong guesses, you lose!\n")
-    prCyan("Let's begin.")
     choose_word()
 
-# Choose a random word from the word list
+"""
+Choose a random word from the word list  
+"""
 def choose_word():
     word = random.choice(word_list).upper()
-    return word
+    play_game(word)
 
+"""
+Message to continuing player 
+"""
 def continue_message():
-    prCyan("\nHello again, " + name.capitalize() + ".\n")
+    prCyan("\nHello again, " + name.capitalize() + ".")
+    prCyan("Guess the letters or the whole word.")
     prCyan("If you make six wrong guesses, you lose!\n")
-    prCyan("Here we go.")
     choose_word()
+    play_game(word)
 
 # # Add colors
 # def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
@@ -65,65 +73,30 @@ def continue_message():
  
 # def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
  
-# # def get_name():
-# #     print("""\n
-# # LET'S PLAY A GAME OF HANGMAN.
-# # """)
-# #     prCyan("""
-# # *****************************""") 
-# #     prRed("""
-# #       -----------      
-# #       |         |      
-# #       |         |      
-# #       |                
-# #       |                
-# #       |                
-# #       |                
-# #       |                
-# #       |    
-# #     """)            
-    
-# #     prCyan("""                        
-# # *****************************\n""")
-# #     new_name = input("Please enter your name:  ")
-# #     main(name)
-
-# # def intro_message():
-# #     prCyan("\nHello, " + name.capitalize() + ".\n")
-# #     prCyan("If you make six wrong guesses, you lose!\n")
-# #     prCyan("Let's begin.")
-
-# # def continue_message():
-# #     prCyan("\nHello again, " + name.capitalize() + ".\n")
-# #     prCyan("If you make six wrong guesses, you lose!\n")
-# #     prCyan("Here we go.")
-
-
-# Play game
-def play_game():
-    # Convert word into a string of its letters
+"""
+Play game  
+"""
+def play_game(word):
+    # Convert word into a list of its letters
     word_letters = list(word)
 
-    # Create list of correct letters in word with no duplicates in order to compare to guessed letters list
+    # Create list of correct letters in word with no 
+    # duplicates in order to compare to guessed letters list
     correct_letters = list(dict.fromkeys(word_letters))
 
     # Calculate number of letters in word
     num_of_letters = len(word)
 
-    #Create a list for positions of letters that match word
+    #Create a list for number positions of letters that match word
     positions = [x for x in range(num_of_letters)]
 
-    # Create blank spaces for each letter in word to show player in console
+    # Create a string of dashes for each letter in word 
     word_string = ["_" for x in range(num_of_letters)]
     display_wordstring = " ".join(word_string)
 
-    # Create list of letters that were guessed
+    # Create variables for guesses
     guessed_letters = []
-
-    # Create list of words that were guessed
     guessed_words = []
-
-    # Create list of correct letters that were guessed
     guessed_correct_letters = []
 
     # Create variable to count the number of wrong attempts
@@ -135,11 +108,13 @@ def play_game():
     while not won and attempts < 6:
         # Create variable for player's guess
         print(display_hangman(attempts))
-        prCyan(name)
         prCyan("The word you're trying to guess has " + str(num_of_letters) + " letters.\n")
+        print("Letters already chosen: ")
+        print(guessed_letters)
+        print("\n")
         prCyan(display_wordstring)
         print("\n")
-        guess = input("Please guess a letter or you can try to guess the word.  ").upper()
+        guess = input("Please guess a letter or you can try to guess the word.\n").upper()
         if guess.isalpha() and len(guess) == 1:
                 if guess in guessed_letters:
                     os.system('clear')
@@ -162,8 +137,6 @@ def play_game():
                     # Test if word is complete
                     guessed_correct_letters.sort()
                     correct_letters.sort()
-                    # print(guessed_correct_letters)
-                    # print(correct_letters)
                     if guessed_correct_letters == correct_letters:
                         won=True
         elif guess.isalpha() and len(guess) > 1:
@@ -180,9 +153,11 @@ def play_game():
         else:
             os.system('clear')
             prCyan("Oops!!\nYou did not enter a letter or word, please try again.\n")
-    game_over(won, attempts)
+    game_over(word, won, attempts)
 
-# Display Hangman based on number of wrong attempts
+"""
+Display Hangman based on number of wrong attempts
+"""  
 def display_hangman(attempts):
     if attempts == 0:
         hangman_result = """
@@ -302,8 +277,10 @@ WRONG ANSWERS: 6/6  YOU LOST!
 *****************************"""
     return hangman_result
 
-# Show win or lost to player
-def game_over(won, attempts):
+"""
+Show win or lost to player  
+"""
+def game_over(word, won, attempts):
     if attempts >= 6:
         os.system('clear')
         print("\n ")
@@ -330,15 +307,15 @@ def game_over(won, attempts):
         """)
         continue_or_quit()
 
-# Ask player to continue playing or quit
+"""
+Ask player to continue playing or quit 
+""" 
 def continue_or_quit():
-    print()
     play_again = False
     while not play_again:
         reply = input("\nWould you like to play again?\nPlease enter Y to play again and N to quit.").upper()
         if reply == "Y":
             os.system('clear')
-            play_again = True
             continue_message()
         elif reply == "N":
             prCyan("\nThank you for playing today.\nGoodbye!\n")
@@ -346,51 +323,13 @@ def continue_or_quit():
         else:
             print("\nYour entry was invalid.\n")
 
+# Name is global variable, intro_message 
+# starts the game for the new player
 name = get_name()
 intro_message()
-word = choose_word()
-play_game()
 
-# # Main function    
-# def main(name):
-# #     print("""\n
-# # LET'S PLAY A GAME OF HANGMAN.
-# # """)
-# #     prCyan("""
-# # *****************************""") 
-# #     prRed("""
-# #       -----------      
-# #       |         |      
-# #       |         |      
-# #       |                
-# #       |                
-# #       |                
-# #       |                
-# #       |                
-# #       |    
-# #     """)            
     
-# #     prCyan("""                        
-# # *****************************\n""")
-# #     name = input("Please enter your name:  ")
-# #     os.system('clear')
-# #     prCyan("\nHello, " + name.capitalize() + ".\n")
-# #     prCyan("If you make six wrong guesses, you lose!\n")
-# #     prCyan("Let's begin.")
-# #     # name = get_name()
-#     word = choose_word()
-#     play_game(word)
 
-# if __name__ == "__main__":
-#     main()
-
-# Main function    
-# def main(name):
-#     word = choose_word()
-#     play_game(word)
-
-# if __name__ == "__main__":
-#     main()
 
 
     
